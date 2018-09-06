4b944a3d1c33459049ca6ded95887813888f44b5
Ashik <aachaparambil@fulcrum-gt.com>
2018-09-06 15:21:13 -0500

    Add project files.

---
+++ CrossplatApp/CrossplatApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrossplatApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {   
+            InitializeComponent();
+            Slider.Value = 0;
+        }
+        public void BtnClick(object sender, EventArgs e) {
+            var btn = (Button)sender;
+            btn.Text = (btn.Text == "clicked")?"click":"clicked";
+
+            
+        }
+        public void Sliding(Object sender , Xamarin.Forms.ValueChangedEventArgs e) {
+            sLabel.Text = string.Format("{0:F6}", e.NewValue);
+        }
+
+        private void Btn_AddBook(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewBook());
+        }
+    }
+}
---
+++ CrossplatApp/CrossplatApp/NewBook.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrossplatApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewBook : ContentPage
+	{
+		public NewBook ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_SaveBook(object sender, EventArgs e)
+        {
+            DisplayAlert("Success", "Book Saved","OK");
+        }
+
+       
+    }
+}
