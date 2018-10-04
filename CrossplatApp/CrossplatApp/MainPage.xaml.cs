9bfa6809b6998979aa2ad5901000b4af6cd98fe0
Ashik Achaparambil <getashik@gmail.com>
2018-10-04 16:03:35 -0500

    added chart, appname change

--- CrossplatApp/CrossplatApp/MainPage.xaml.cs
+++ CrossplatApp/CrossplatApp/MainPage.xaml.cs
@@ -12,26 +12,32 @@ using System.IO;
 using System.Reflection;
 using CrossplatApp.Models;
 using Newtonsoft.Json;
+using mEntry = Microcharts.Entry;
+
+
 
 
 namespace CrossplatApp
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
             Slider.Value = 0;
-
-
-
-
+            DisplayWchart();
         }
 
         public void BtnClick(object sender, EventArgs e)
         {
             var btn = (Button)sender;
             btn.Text = (btn.Text == "clicked") ? "click" : "clicked";
+            var MinimumDate = new DateTime(2018, 7, 15);
+            var MaximumDate = new DateTime(2019, 4, 10);
+            //DisplayAlert("cc", (MaximumDate - MinimumDate).TotalDays.ToString() + "----" + (MaximumDate - DateTime.Today).TotalDays, "cc");
+            DisplayAlert("dd", (MaximumDate - MinimumDate).TotalDays.ToString(), "ff");
+
 
         }
         public void ListsFamily()
@@ -121,6 +127,58 @@ namespace CrossplatApp
         private void Btn_ApiList(object sender, EventArgs e)
         {
             Navigation.PushAsync(new ApiList.ApiList());
+        }
+
+        public void DisplayWchart()
+        {
+
+            List<mEntry> entries = new List<mEntry>
+            {
+                //new mEntry(200) {
+                //Label = "January",
+                //ValueLabel = "200",
+                //Color = SkiaSharp.SKColor.Parse("FF0000")
+                //},
+                ////new mEntry(400) {
+                ////Label = "February",
+                ////ValueLabel = "400",
+                ////Color = SkiaSharp.SKColor.Parse("#00bfff")
+                ////},
+                //new mEntry(200) {
+                //Label = "March",
+                //ValueLabel = "-100",
+                //Color = SkiaSharp.SKColor.Parse("#90D585")
+                //}
+            };
+
+            var MinimumDate = new DateTime(2018, 7, 15);
+            var MaximumDate = new DateTime(2019, 4, 10);
+            // DisplayAlert("cc", (MaximumDate - MinimumDate).TotalDays.ToString() + "----" + (MaximumDate - DateTime.Today).TotalDays, "cc");
+
+            int tw = System.Convert.ToInt32((MaximumDate - DateTime.Today).TotalDays.ToString());
+            int wd = System.Convert.ToInt32((DateTime.Today - MinimumDate).TotalDays.ToString());
+
+            entries.Add(new mEntry(tw)
+            {
+                Label = "Yet",
+                ValueLabel = tw.ToString(),
+                Color = SkiaSharp.SKColor.Parse("#00bfff")
+            });
+
+            entries.Add(new mEntry(wd)
+            {
+                Label = "Done",
+                ValueLabel = wd.ToString(),
+                Color = SkiaSharp.SKColor.Parse("#008000")
+            });
+            wChart.Chart = new Microcharts.DonutChart
+            {
+                Entries = entries,
+
+            };
+
+
+
         }
     }
 }
